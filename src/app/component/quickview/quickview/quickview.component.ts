345a5aa3ed2ca6de5cd66b79493d3a6972d15303
payalbanode0 <payalbanode0@gmail.com>
2022-06-30 19:43:41 +0530

    added add feedback

--- src/app/component/quickview/quickview/quickview.component.ts
+++ src/app/component/quickview/quickview/quickview.component.ts
@@ -9,7 +9,7 @@ import { BookserviceService } from 'src/app/services/bookservice/bookservice.ser
 })
 export class QuickviewComponent implements OnInit {
 
-
+  feedback: any
   BookId: any;
   bookArray: any;
   comment: any;
@@ -28,12 +28,33 @@ export class QuickviewComponent implements OnInit {
     this.bookService.getbook().subscribe((response: any) => {
       response.response.forEach((element: any) => {
         if (element.bookId == this.BookId) {
-          this. bookArray= element;
+          this.bookArray = element;
 
           console.log(this.bookArray);
         }
       });
     })
+  }
+  addFeedback() {
+    let data = {
+      rating: this.rating,
+      comment: this.comment,
+      bookId: this.BookId
+    }
+    console.log("reqdata")
+    this.bookService.addfeedback(data).subscribe((response: any) => {
+      console.log("User Feedback", response);
+      this.getFeedback();
+
+    }, error => { console.log(error) })
+  }
+  getFeedback() {
+    console.log(this.bookArray)
+    this.bookService.getfeedback(this.bookArray.bookId).subscribe((response: any) => {
+      console.log('User Feedback', response);
+      this.bookArray = response.response;
+      console.log(this.bookArray);
+    });
   }
 
 }
--- src/app/services/bookservice/bookservice.service.ts
+++ src/app/services/bookservice/bookservice.service.ts
@@ -8,12 +8,12 @@ import { HttpServiceService } from '../httpService/http-service.service';
 export class BookserviceService {
   token: any;
 
-  constructor(private httpservice:HttpServiceService) { 
+  constructor(private httpservice: HttpServiceService) {
     this.token = localStorage.getItem("token")
 
   }
   getbook() {
- this.token = localStorage.getItem("token")
+    this.token = localStorage.getItem("token")
     console.log();
     let headerOption = {
       headers: new HttpHeaders({
@@ -22,5 +22,28 @@ export class BookserviceService {
       })
     }
     return this.httpservice.getServices('Book/GetAllBook', true, headerOption);
+  }
+  addfeedback(data:any) {
+    this.token = localStorage.getItem('token')
+    console.log();
+    let headerOption = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json-patch+json',
+        'Authorization': 'Bearer ' + this.token
+      })
+    }
+    console.log("data")
+    return this.httpservice.postService('api/Feedback/AddFeedback', data,true,headerOption);
+  }
+  getfeedback(data:any) {
+    this.token = localStorage.getItem("token")
+    console.log(data);
+    let headerOption = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.token
+      })
+    }
+    return this.httpservice.postService(`api/Feedback/Get${data.BookId}`,data, true,headerOption);
   }
 }
